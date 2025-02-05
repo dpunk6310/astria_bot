9db5ea997f15ed8007cfad39c42c88e4990470cd
dpunk6310 <dpunk6310@gmail.com>
2025-02-05 15:16:07 +0300

    add downloads photos

--- app/bot/handlers/user/start.py
+++ app/bot/handlers/user/start.py
@@ -1,12 +1,15 @@
 from pathlib import Path
+from typing import List
+import os
 
 from aiogram import types, Router
 from aiogram.filters import CommandStart
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, FSInputFile
+from aiogram.types import FSInputFile
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from data.messages import use_messages
 from core.backend.api import create_user_db
+from loader import bot
 
 
 user_router = Router()
@@ -36,10 +39,24 @@ async def start_handler(message: types.Message, messages):
         ),
     )
     
-    photo_path = BASE_DIR / "logo_p.png"
+    photo_path = BASE_DIR / "media/logo_p.png"
     
     await message.answer_photo(
         photo=FSInputFile(photo_path),
         caption=messages["start"],
         reply_markup=builder.as_markup()
     )
+    
+
+@user_router.message()
+async def handle_albums(message: types.Message):
+    photos = message.photo
+    if photos:
+        if not os.path.exists(BASE_DIR / "media" / "photos"):
+            os.makedirs(BASE_DIR / "media/photos/")
+        photo = await bot.get_file(photos[-1].file_id)
+        await message.bot.download_file(photo.file_path, destination=BASE_DIR / "media" / photo.file_path)
+        await bot.send_photo(
+            message.chat.id, FSInputFile(BASE_DIR / "media" / photo.file_path), caption="Вот оно"
+        )
+        # os.remove(photo.file_path)
--- app/bot/main.py
+++ app/bot/main.py
@@ -1,17 +1,15 @@
 import asyncio
-from pathlib import Path
-
-import loader
-from data import config
-
-from handlers.user.start import user_router
 
 from loguru import logger as log
+
+import loader
+from handlers.user.start import user_router
 
 
 async def on_startup():
     loader.dp.include_router(user_router)
     await loader.dp.start_polling(loader.bot)
+    
     
 
 if __name__ == '__main__':
