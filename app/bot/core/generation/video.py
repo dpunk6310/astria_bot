2672553fc0c88b275cbf857fdf464428c2b9254e
dpunk6310 <dpunk6310@gmail.com>
2025-02-22 20:29:14 +0200

    update

--- app/bot/core/generation/video.py
+++ app/bot/core/generation/video.py
@@ -1,10 +1,10 @@
 import httpx
 import asyncio
-import logging
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from core.logger.logger import get_logger
+
+
+log = get_logger()
 
 API_URL = "https://queue.fal.run/fal-ai/kling-video/v1.6/pro/image-to-video"
 HEADERS = {
@@ -22,14 +22,15 @@ async def generate_video_from_image(image_url: str) -> str:
         async with httpx.AsyncClient() as client:
             # Шаг 1: Отправка изображения в API
             res = await client.post(url=API_URL, json=data, headers=HEADERS)
-            res.raise_for_status()
-            
+            if not res or res.status_code == 400:
+                log.error(f"ошибка Отправка изображения в API {res.text}")
+                return None
             out_data = res.json()
             status_url = out_data.get("status_url")
             response_url = out_data.get("response_url")
             
             if not status_url or not response_url:
-                logger.error("Не удалось получить URL-адреса из ответа API.")
+                log.error("Не удалось получить URL-адреса из ответа API.")
                 return None
             
             # Шаг 2: Ожидание завершения обработки
@@ -37,40 +38,38 @@ async def generate_video_from_image(image_url: str) -> str:
             while True:
                 await asyncio.sleep(10)  # Используем asyncio.sleep вместо time.sleep
                 r2 = await client.get(url=status_url, headers=HEADERS)
+                if not r2 or r2.status_code == 400:
+                    log.error(f"Ошибка Ожидание завершения обработки {res.text}")
                 if not r2.is_success:
-                    logger.error("Ошибка при получении статуса обработки.")
+                    log.error("Ошибка при получении статуса обработки.")
                     return None
                 
                 r2_data = r2.json()
                 status = r2_data.get("status")
                 
                 if status in ["IN_PROGRESS", "IN_QUEUE"]:
-                    logger.info(f"Попытка {i}... Видео еще создается.")
+                    log.info(f"Попытка {i}... Видео еще создается.")
                     i += 1
                     continue
                 elif status == "COMPLETED":
                     break
                 else:
-                    logger.error(f"Неизвестный статус обработки: {status}")
+                    log.error(f"Неизвестный статус обработки: {status}")
                     return None
             
             # Шаг 3: Получение результата
             response = await client.get(response_url, headers=HEADERS)
-            response.raise_for_status()
+            if not response or response.status_code == 400:
+                log.error(f"Ошибка запроса на получени видео {response.text}")
+                return None
             res_data = response.json()
-            print(res_data)
             video_url = res_data.get("video", {}).get("url")
             if not video_url:
-                logger.error("Не удалось получить ссылку на видео.")
+                log.error("Не удалось получить ссылку на видео.")
                 return None
             
             return video_url
-    
-    except httpx.RequestError as e:
-        logger.error(f"Ошибка запроса: {e}")
-    except ValueError as e:
-        logger.error(f"Ошибка обработки данных: {e}")
     except Exception as e:
-        logger.error(f"Непредвиденная ошибка: {e}", exc_info=True)
+        log.error(f"Непредвиденная ошибка: {e}", exc_info=True)
     
     return None
--- app/bot/handlers/info.py
+++ app/bot/handlers/info.py
@@ -46,7 +46,7 @@ async def start_handler(message: types.Message, messages):
     )
     
     keyboard = builder.as_markup()
-    if user_db.get("count_generations") > 3:
+    if user_db.get("count_generations", 0) > 3:
         keyboard = get_main_keyboard()
     
     asyncio.create_task(create_referal(user_db, message))
