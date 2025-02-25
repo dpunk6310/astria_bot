cd5d7b1e9b47ad83a9691bc2f9f048dfee37fa89
dpunk6310 <dpunk6310@gmail.com>
2025-02-25 23:27:36 +0200

    update

--- app/bot/core/generation/photo.py
+++ app/bot/core/generation/photo.py
@@ -15,9 +15,17 @@ headers = {
 log = get_logger()
 
 
-async def learn_model_api(images: list[str], gender: str):
+async def learn_model_api(images: list[str], gender: str) -> dict:
+    """ Обучает модель на основе переданных изображений и указанного пола.
+    """
+    model_title = str(uuid4())
+    attempts = 0
+    delay: int = 5
+    max_attempts: int = 5
+
+    # Подготовка данных для запроса
     data = {
-        "tune[title]": str(uuid4()),
+        "tune[title]": model_title,
         "tune[name]": gender,
         "tune[base_tune_id]": 1504944,
         "tune[model_type]": "lora",
@@ -26,19 +34,52 @@ async def learn_model_api(images: list[str], gender: str):
         "tune[steps]": 400,
         "tune[callback]": "https://webhook.site/f9674fa9-1bd3-4e31-b6b5-624dd7f045e9",
     }
+
     files = []
     for image in images:
-        image_data = load_image(image)  
-        files.append(("tune[images][]", image_data))
-        os.remove(image)
-                        
-    async with httpx.AsyncClient() as client:
         try:
-            response = await client.post(API_URL, data=data, files=files, headers=headers)
+            image_data = load_image(image)  
+            files.append(("tune[images][]", image_data))
+            log.info(f"Изображение {image} успешно загружено для обучения модели.")
         except Exception as err:
-            log.error(f"Ошибка обучения: {err}")
-            return None
-        return response.json()
+            log.error(f"Ошибка загрузки изображения {image}: {err}")
+            continue 
+        finally:
+            try:
+                os.remove(image)
+                log.info(f"Изображение {image} удалено после загрузки.")
+            except Exception as err:
+                log.error(f"Ошибка удаления изображения {image}: {err}")
+
+    if not files:
+        log.error("Нет доступных изображений для обучения модели.")
+        return None
+
+    log.info(f"Начало обучения модели с названием '{model_title}' для пола '{gender}'.")
+
+    async with httpx.AsyncClient() as client:
+        while attempts < max_attempts:
+            attempts += 1
+            try:
+                log.info(f"Попытка {attempts}/{max_attempts}: отправка запроса на обучение модели...")
+                response = await client.post(API_URL, data=data, files=files, headers=headers)
+                response.raise_for_status() 
+                return response.json()
+            except httpx.HTTPStatusError as err:
+                log.error(f"Ошибка HTTP при обучении модели: {err}")
+            except httpx.RequestError as err:
+                log.error(f"Ошибка соединения при обучении модели: {err}")
+            except Exception as err:
+                log.error(f"Неожиданная ошибка при обучении модели: {err}")
+
+            if attempts < max_attempts:
+                log.info(f"Повторная попытка через {delay} секунд...")
+                await asyncio.sleep(delay)
+            else:
+                log.warning(f"Достигнуто максимальное количество попыток ({max_attempts}). Обучение модели прервано.")
+
+    return None
+
 
 
 def load_image(file_path):
@@ -46,13 +87,12 @@ def load_image(file_path):
         return f.read()
     
     
-async def generate_images(tune_id: int, promt: str, effect: str = None):
+async def generate_images(tune_id: int, prompt: str, effect: str = None) -> dict:
     attempts = 0
     delay = 8
     max_attempts = 1000
     data = {
-        'prompt[text]': f'<lora:{tune_id}:1> {promt}',
-        # 'prompt[steps]': 40,
+        'prompt[text]': f'<lora:{tune_id}:1> {prompt}',
         'prompt[super_resolution]': "true",
         'prompt[inpaint_faces]': "true",
         'prompt[num_images]': 3,
@@ -61,18 +101,32 @@ async def generate_images(tune_id: int, promt: str, effect: str = None):
     }
     if effect is not None:
         data['prompt[style]'] = effect
+
+    log.info(f"Начало генерации изображений для tune_id={tune_id} с промтом: '{prompt}'")
+
     while attempts < max_attempts:
         async with httpx.AsyncClient() as client:
             try:
-                response = await client.post(f"https://api.astria.ai/tunes/{tune_id}/prompts", data=data, headers=headers)
-                if response and response.status_code not in [400, 502, 404]:
-                    return response.json()
-                attempts += 1
+                log.info(f"Попытка {attempts + 1}/{max_attempts}: отправка запроса на генерацию изображений")
+                response = await client.post(
+                    f"https://api.astria.ai/tunes/{tune_id}/prompts",
+                    data=data,
+                    headers=headers
+                )
+                response.raise_for_status()
+                log.info("Запрос успешно выполнен, получен ответ от сервера")
+                return response.json()
+            except httpx.HTTPStatusError as err:
+                log.error(f"Ошибка HTTP при запросе: {err}")
             except Exception as err:
-                log.error(f"Ошибка получения ответа от отправки на генерации фото: {err} {response.text}")
-                await asyncio.sleep(delay)
+                log.error(f"Неожиданная ошибка при запросе: {err}")
+            finally:
                 attempts += 1
-                continue
+                if attempts < max_attempts:
+                    log.info(f"Повторная попытка через {delay} секунд...")
+                    await asyncio.sleep(delay)
+                else:
+                    log.warning("Достигнуто максимальное количество попыток. Генерация изображений прервана.")
     return None
                     
 
@@ -82,58 +136,90 @@ def load_image(file_path):
         return f.read()
     
     
-async def wait_for_generation(prompt_id):
-    """Асинхронное ожидание завершения генерации изображения"""
+async def wait_for_generation(prompt_id: int):
+    """ Асинхронное ожидание завершения генерации изображения по указанному prompt_id.
+    """
     attempts = 0
-    delay = 8
-    max_attempts = 1000
+    delay: int = 8
+    max_attempts: int = 1000
+
+    log.info(f"Начало ожидания завершения генерации изображения для prompt_id={prompt_id}.")
 
     while attempts < max_attempts:
+        attempts += 1
         try:
+            log.info(f"Попытка {attempts}/{max_attempts}: проверка статуса генерации...")
+            
             async with httpx.AsyncClient() as client:
                 status_response = await client.get(
                     f"https://api.astria.ai/prompts/{prompt_id}",
                     headers=headers
                 )
-                status_response.raise_for_status()
+                status_response.raise_for_status() 
                 status_data = status_response.json()
-                if status_data.get('images') and len(status_data['images']) > 0:
-                    return status_data['images']
 
-                if status_data.get('status') == 'failed':
-                    return None
+            if status_data.get('images') and len(status_data['images']) > 0:
+                log.info(f"Генерация изображения для prompt_id={prompt_id} успешно завершена.")
+                return status_data['images']
 
-                await asyncio.sleep(delay)
-                attempts += 1
+            if status_data.get('status') == 'failed':
+                log.error(f"Генерация изображения для prompt_id={prompt_id} завершилась с ошибкой.")
+                return None
 
-        except Exception as err:
-            log.error(f"Ошибка получения ответа от ожидания генерации фото: {err} {status_response.text}")
+            log.info(f"Генерация изображения для prompt_id={prompt_id} еще не завершена. Повторная проверка через {delay} секунд...")
             await asyncio.sleep(delay)
-            attempts += 1
+
+        except httpx.HTTPStatusError as err:
+            log.error(f"Ошибка HTTP при проверке статуса генерации: {err}")
+        except httpx.RequestError as err:
+            log.error(f"Ошибка соединения при проверке статуса генерации: {err}")
+        except Exception as err:
+            log.error(f"Неожиданная ошибка при проверке статуса генерации: {err}")
+
+        if attempts < max_attempts:
+            await asyncio.sleep(delay)
+        else:
+            log.warning(f"Достигнуто максимальное количество попыток ({max_attempts}). Генерация изображения не завершена.")
+
+    return None
 
 
-async def wait_for_training(tune_id: int):
+async def wait_for_training(tune_id: int) -> bool:
+    """ Ожидает завершения обучения модели по указанному tune_id.
+    """
     attempts = 0
-    max_attempts = 2000
-    delay = 30  
+    delay: int = 30
+    max_attempts: int = 2000
+
+    log.info(f"Начало ожидания завершения обучения модели с tune_id={tune_id}.")
 
     while attempts < max_attempts:
+        attempts += 1
         try:
+            log.info(f"Попытка {attempts}/{max_attempts}: проверка статуса обучения...")
             
             async with httpx.AsyncClient() as client:
                 response = await client.get(f"https://api.astria.ai/tunes/{tune_id}", headers=headers)
-                response.raise_for_status()
+                response.raise_for_status() 
                 status_data = response.json()
 
             if status_data.get("trained_at") is not None:
+                log.info(f"Обучение модели с tune_id={tune_id} успешно завершено.")
                 return True
 
+            log.info(f"Обучение модели с tune_id={tune_id} еще не завершено. Повторная проверка через {delay} секунд...")
             await asyncio.sleep(delay)
-            attempts += 1
 
+        except httpx.HTTPStatusError as err:
+            log.error(f"Ошибка HTTP при проверке статуса обучения: {err}")
+        except httpx.RequestError as err:
+            log.error(f"Ошибка соединения при проверке статуса обучения: {err}")
         except Exception as err:
-            log.error(f"Ошибка получения ответа от обучения модели: {err} {response.text}")
+            log.error(f"Неожиданная ошибка при проверке статуса обучения: {err}")
+
+        if attempts < max_attempts:
             await asyncio.sleep(delay)
-            attempts += 1
+        else:
+            log.warning(f"Достигнуто максимальное количество попыток ({max_attempts}). Обучение модели не завершено.")
 
     return False
